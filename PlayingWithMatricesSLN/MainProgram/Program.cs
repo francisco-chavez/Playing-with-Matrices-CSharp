c665e6f13df19f920aa82ca112b761331f80c140
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2021-02-14 20:08:55 -0800

    Started implementing the Strassen Matrix Multiplication Algorithm.

--- PlayingWithMatricesSLN/MainProgram/Program.cs
+++ PlayingWithMatricesSLN/MainProgram/Program.cs
@@ -126,11 +126,12 @@ namespace MainProgram
 				for (int j = 0; j < sizeB.Item2; j++)
 					matrixB_T[j * sizeB.Item1 + i] = matrixB[i * sizeB.Item2 + j];
 
-			var resultSize = resultRowCount * resultColumnCount;
-			var result = new float[resultSize];
+			var resultSize	= resultRowCount * resultColumnCount;
+			var result		= new float[resultSize];
 
-			int rI = 0;
-			int aRowOffset = 0;
+			int rI			= 0;
+			int aRowOffset	= 0;
+
 			for (int rY = 0; rY < resultRowCount; rY++)
 			{
 				int bRowOffset = 0;
@@ -154,7 +155,273 @@ namespace MainProgram
 
 		public static float[] MatrixMult_MonoArray_Strassen(float[] matrixA, float[] matrixB, Tuple<int, int> sizeA, Tuple<int, int> sizeB)
 		{
+			if (sizeA.Item2 != sizeB.Item1)
+				throw new Exception();
+
+			var resultRowCount		= sizeA.Item1;
+			var resultColumnCount	= sizeB.Item2;
+			var sharedDimension		= sizeA.Item2;
+
+			var maxD = sizeA.Item1 > sizeA.Item2 ? sizeA.Item2 : sizeA.Item1;
+			maxD = maxD < sizeB.Item2 ? sizeB.Item2 : maxD;
+
+			int n = 0;
+			int t = 1;
+
+			while (t < maxD)
+			{
+				t *= 2;
+				n++;
+			}
+
+			var halfT = t / 2;
+
+			var start_11_0 = 0;
+			var start_11_1 = 0;
+			//var end_11_0 = (halfT) - 1;
+			//var end_11_1 = (halfT) - 1;
+
+			var start_12_0 = 0;
+			var start_12_1 = halfT;
+			//var end_12_0 = (halfT) - 1;
+			//var end_12_1 = t - 1;
+
+			var start_21_0 = halfT;
+			var start_21_1 = 0;
+			//var end_21_0 = t - 1;
+			//var end_21_1 = (halfT) - 1;
+
+			var start_22_0 = halfT;
+			var start_22_1 = halfT;
+			//var end_22_0 = t - 1;
+			//var end_22_1 = t - 1;
+
+			int blockSize = halfT * halfT;
+
+			var a11 = new float[blockSize];
+			var a12 = new float[blockSize];
+			var a21 = new float[blockSize];
+			var a22 = new float[blockSize];
+
+			var b11 = new float[blockSize];
+			var b12 = new float[blockSize];
+			var b21 = new float[blockSize];
+			var b22 = new float[blockSize];
+
+			FillInBlock(a11, matrixA, start_11_1, start_11_0, halfT, sizeA.Item2, sizeA.Item1);
+			FillInBlock(a12, matrixA, start_12_1, start_12_0, halfT, sizeA.Item2, sizeA.Item1);
+			FillInBlock(a21, matrixA, start_21_1, start_21_0, halfT, sizeA.Item2, sizeA.Item1);
+			FillInBlock(a22, matrixA, start_22_1, start_22_0, halfT, sizeA.Item2, sizeA.Item1);
+
+			FillInBlock(b11, matrixB, start_11_1, start_11_0, halfT, sizeB.Item2, sizeB.Item1);
+			FillInBlock(b12, matrixB, start_12_1, start_12_0, halfT, sizeB.Item2, sizeB.Item1);
+			FillInBlock(b21, matrixB, start_21_1, start_21_0, halfT, sizeB.Item2, sizeB.Item1);
+			FillInBlock(b22, matrixB, start_22_1, start_22_0, halfT, sizeB.Item2, sizeB.Item1);
+
+			var m1 = new float[blockSize];
+			var m2 = new float[blockSize];
+			var m3 = new float[blockSize];
+			var m4 = new float[blockSize];
+			var m5 = new float[blockSize];
+			var m6 = new float[blockSize];
+			var m7 = new float[blockSize];
+
+			var tm1 = new float[blockSize];
+			var tm2 = new float[blockSize];
+
+			///
+			/// Calculte m1 to m7
+			/// 
+			Add(a11, a22, tm1, blockSize);
+			Add(b11, b22, tm2, blockSize);
+			// Mult(tm1, tm2) => m1;
+
+			Add(a21, a22, tm1, blockSize);
+			// Mult(tm1, b11) => m2;
+
+
+			Subtract(b12, b22, tm2, blockSize);
+			// Mult(a11, tm2) => m3;
+
+			Subtract(b21, b11, tm2, blockSize);
+			// Mult(a22, tm2) => m4
+
+			Add(a11, a12, tm1, blockSize);
+			// Mult(tm1, b22) => m5
+
+			Subtract(a21, a11, tm1, blockSize);
+			Add(b11, b12, tm2, blockSize);
+			// Mult(tm1, tm2) => m6
+
+			Subtract(a12, a22, tm1, blockSize);
+			Add(b21, b22, tm2, blockSize);
+			// Mult(tm1, tm2) => m7
+
+
+			float[] c11 = new float[blockSize];
+			float[] c12 = new float[blockSize];
+			float[] c21 = new float[blockSize];
+			float[] c22 = new float[blockSize];
+
+			///
+			/// Calculate c11, c12, c21, c22
+			/// 
+			Add(m1, m2, tm1, blockSize);
+			Subtract(m7, m5, tm2, blockSize);
+			Add(tm1, tm2, c11, blockSize);
+
+			Add(m3, m5, c12, blockSize);
+
+			Add(m2, m4, c21, blockSize);
+
+			Subtract(m1, m2, tm1, blockSize);
+			Add(m3, m6, tm2, blockSize);
+			Add(tm1, tm2, c22, blockSize);
+
+			float[] result = new float[resultRowCount * resultColumnCount];
+
+			///
+			/// Transfer { c11, c12, c21, c22 } to result
+			/// 
+			for (int i = 0; i < halfT; i++)
+			{
+				if (i >= resultRowCount)
+					break;
+
+				int offsetR = i * resultColumnCount;
+				int offsetC = i * halfT;
+				for (int j = 0; j < halfT; j++)
+				{
+					if (j >= resultColumnCount)
+						break;
+
+					result[offsetR + j] = c11[offsetC + j];
+				}
+			}
+
+			for (int i = 0; i < halfT; i++)
+			{
+				if (i >= resultRowCount)
+					break;
+
+				int offsetR = (i * resultColumnCount) + halfT;
+				int offsetC = i * halfT;
+
+				for (int j = 0; j < halfT; j++)
+				{
+					if (j + halfT >= resultColumnCount)
+						break;
+
+					result[offsetR + j] = c12[offsetC + j];
+				}
+			}
+
+			for (int i = 0; i < halfT; i++)
+			{
+				if (i + halfT > resultRowCount)
+					break;
+
+				int offsetR = (i + halfT) * resultColumnCount;
+				int offsetC = i * halfT;
+
+				for (int j = 0; j < halfT; j++)
+				{
+					if (j >= resultColumnCount)
+						break;
+
+					result[offsetR + j] = c21[offsetC + j];
+				}	
+			}
+
+			for (int i = 0; i < halfT; i++)
+			{
+				if (i + halfT >= resultRowCount)
+					break;
+
+				int offsetR = ((i + halfT) * resultColumnCount) + halfT;
+				int offsetC = i * halfT;
+
+				for (int j = 0; j < halfT; j++)
+				{
+					if (j + halfT >= resultColumnCount)
+						break;
+
+					result[offsetR + j] = c22[offsetC + j];
+				}
+			}
+
 			throw new NotImplementedException();
+
+			//return result;
+		}
+
+		public static void FillInBlock(float[] result, float[] source, int startColumn, int startRow, int length, int sourceWidth, int sourceHeight)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				int currentRowSource = i + startRow;
+
+				for (int j = 0; j < length; j++)
+				{
+					float value;
+
+					var indexR = (i * length) + j;
+
+					int currentColumnSource = j + startColumn;
+
+					if (currentColumnSource < sourceWidth && currentRowSource < sourceHeight)
+					{
+						var indexS = (currentRowSource * sourceWidth) + currentColumnSource;
+						value = source[indexS];
+					}
+					else
+					{
+						value = 0.0f;
+					}
+
+					result[indexR] = value;
+				}
+			}
+		}
+
+		/// <summary>
+		/// This will copy a section of <paramref name="source"/> into <paramref name="result"/>.
+		/// </summary>
+		/// <param name="result">This is a matrix that's <paramref name="length"/> by <paramref name="length"/> in size. We will copy our data into this matrix.</param>
+		/// <param name="source">This is a matrix that's 2 * <paramref name="length"/> by 2 * <paramref name="length"/> in size. This is the matrix we will by copying from.</param>
+		/// <param name="startColumn">The column in the source matrix that we start coping data from.</param>
+		/// <param name="startRow">The row in the source matrix that we start coping data from.</param>
+		/// <param name="length">The dimensional size reference used in our matrices.</param>
+		public static void FillInBlock(float[] result, float[] source, int startColumn, int startRow, int length)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				int currentRowSource = i + startRow;
+				int offsetRowResult = i * length;
+				int offsetRowSource = (2 * length) * (currentRowSource + i);
+
+				for (int j = 0; j < length; j++)
+				{
+					var indexR = offsetRowResult + j;
+
+					int currentColumnSource = j + startColumn;
+					var indexS = offsetRowSource + currentColumnSource;
+
+					result[indexR] = source[indexS];
+				}
+			}
+		}
+
+		public static void Add(float[] left, float[] right, float[] result, int length)
+		{
+			for (int i = 0; i < length; i++)
+				result[i] = left[i] + right[i];
+		}
+
+		public static void Subtract(float[] left, float[] right, float[] result, int length)
+		{
+			for (int i = 0; i < length; i++)
+				result[i] = left[i] - right[i];
 		}
 
 
